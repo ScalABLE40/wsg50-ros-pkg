20e9a6b4ea5fb2455d6716c82af4390fbfb1f749
DavidWuthier <dwuthier@gmail.com>
2018-01-25 17:29:25 +0100

    Retrofit to hardware revision 1

---
+++ wsg_50_driver/bash/open_close.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rosservice call /wsg_50_driver/move "width: 100.0
+speed: 400.0"
+rosservice call /wsg_50_driver/move "width: 10.0
+speed: 400.0"
+rosservice call /wsg_50_driver/move "width: 100.0
+speed: 400.0"
+rosservice call /wsg_50_driver/move "width: 10.0
+speed: 400.0"
+rosservice call /wsg_50_driver/move "width: 100.0
+speed: 400.0"
+rosservice call /wsg_50_driver/move "width: 10.0
+speed: 400.0"
+rosservice call /wsg_50_driver/move "width: 100.0
+speed: 400.0"
+rosservice call /wsg_50_driver/move "width: 10.0
+speed: 400.0"
